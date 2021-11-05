7dbba89a22bde791a48428a132e52007f6db545f
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2021-11-05 11:26:58 -0700

    stuck on task 4 dont quite understand how to implement split method, will comeback later

--- index.js
+++ index.js
@@ -245,9 +245,13 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(array) {
-  /*Your Code Here*/
+function get20s(array){
+  artists.split('1900 - 2000'); {
+    return(artists.name)
+  }
 }
+
+  
 
 
 
@@ -260,8 +264,13 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(array, number) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  // for(let i = 0; i < array.length; i++){
+  //   if(){
+
+  //   }
+  // }
+  // return length
 }
 
 
